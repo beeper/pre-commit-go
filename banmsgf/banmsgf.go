d4cb8b40af1a5fefdb0ae9c76a8c7b3d3446cc5c
Sumner Evans <sumner@beeper.com>
2023-12-23 16:52:48 -0700

    parse the AST to find instances of Msgf
    Signed-off-by: Sumner Evans <sumner@beeper.com>

--- banmsgf/banmsgf.go
+++ banmsgf/banmsgf.go
@@ -1,33 +1,83 @@
 package main
 
 import (
-	"bufio"
+	"bytes"
 	"fmt"
-	"log"
+	"go/ast"
+	"go/parser"
+	"go/token"
 	"os"
+	"slices"
 	"strings"
 )
 
+const (
+	COLOR_BOLD  = "\033[1m"
+	COLOR_RED   = "\033[31m"
+	COLOR_RESET = "\033[0m"
+)
+
+func checkFile(filename string) (msgfPositions []token.Position) {
+	fset := token.NewFileSet()
+	f, err := parser.ParseFile(fset, filename, nil, parser.ParseComments)
+	if err != nil {
+		panic(err)
+	}
+
+	ast.Inspect(f, func(n ast.Node) bool {
+		if callExpr, ok := n.(*ast.CallExpr); ok {
+			if selector, ok := callExpr.Fun.(*ast.SelectorExpr); ok {
+				if selector.Sel.Name == "Msgf" {
+					pos := fset.Position(n.Pos())
+					msgfPositions = append(msgfPositions, pos)
+				}
+			}
+		}
+		return true
+	})
+
+	ignoreLines := map[int]struct{}{}
+	for _, comment := range f.Comments {
+		if strings.Contains(comment.Text(), "zerolog-allow-msgf") {
+			commentLine := fset.Position(comment.Pos()).Line
+			ignoreLines[commentLine] = struct{}{}
+		}
+	}
+
+	if len(ignoreLines) == 0 {
+		return msgfPositions
+	}
+
+	msgfPositions = slices.DeleteFunc(msgfPositions, func(pos token.Position) bool {
+		_, ok := ignoreLines[pos.Line]
+		return ok
+	})
+	return
+}
+
 func main() {
 	found_msgf := false
+	msgfLines := map[string][]token.Position{}
 	for _, file := range os.Args[1:] {
-		if !strings.HasSuffix(file, ".go") {
+		msgfLines[file] = checkFile(file)
+	}
+
+	for file, positions := range msgfLines {
+		if len(positions) == 0 {
 			continue
 		}
 
-		f, err := os.Open(file)
+		contents, err := os.ReadFile(file)
 		if err != nil {
-			log.Fatal(err)
+			panic(err)
 		}
-		defer f.Close()
+		lines := bytes.Split(contents, []byte("\n"))
 
-		scanner := bufio.NewScanner(f)
-		for i := 1; scanner.Scan(); i++ {
-			if strings.Contains(scanner.Text(), ".Msgf(") {
-				text := scanner.Text()
-				fmt.Printf("%s:%d: %s\n", file, i, text[:len(text)-1])
-				found_msgf = true
-			}
+		fmt.Printf("%s%s:%s\n", COLOR_BOLD, file, COLOR_RESET)
+		for _, pos := range positions {
+			line := lines[pos.Line-1]
+			msgfIdx := bytes.Index(line, []byte("Msgf"))
+			fmt.Printf("%d: %s%s%sMsgf%s%s\n", pos.Line, line[:msgfIdx], COLOR_BOLD, COLOR_RED, COLOR_RESET, line[msgfIdx+4:])
 		}
 	}
 
